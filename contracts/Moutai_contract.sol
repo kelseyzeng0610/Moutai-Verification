7dd9dc463e2d1d542be5a236993386e1a2cb498d
kelseyzeng0610 <zengqd@bc.edu>
2023-03-31 15:29:57 -0400

    change

---
+++ contracts/Moutai_contract.sol
@@ -0,0 +1,244 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+/// @title Asset
+/// @dev Store & retrieve value of a property
+
+contract Asset {
+    address public creatorAdmin;
+    enum Status {NotExist, Pending, Approved, Rejected}
+    enum Role {Visitor, User, Admin, SuperAdmin}
+    event OwnershipTransferAccepted(uint256 indexed propertyId, address indexed newOwner);
+    event OwnershipTransferInitiated(uint256 indexed propertyId, address indexed newOwner);
+    // Struct to store all property related details
+    struct BottleDetail {
+        Status status;
+        uint256 value;
+        address currOwner;
+    }
+
+    mapping(uint256 => BottleDetail) public properties; // Stores all properties propId -> property Detail
+    mapping(uint256 => address) public propOwnerChange; // Keeps track of property owner propId -> Owner Address
+    mapping(address => Role) public userRoles; // Keeps track of user roles
+    mapping(address => bool) public verifiedUsers; // Keeps track of verified user, userId -> verified (true / false)
+    mapping(uint256 => address) public pendingOwner;
+
+    // Modifier to ensure only the property owner access
+    // a specific property
+    modifier onlyOwner(uint256 _propId) {
+        require(properties[_propId].currOwner == msg.sender);
+        _;
+    }
+
+    // Modifier to ensure only the verified user access
+    // a specific property
+    modifier verifiedUser(address _user) {
+        require(verifiedUsers[_user]);
+        _;
+    }
+
+    // Modifier to ensure only the verified admin access a function
+    modifier verifiedAdmin() {
+        require(
+            userRoles[msg.sender] >= Role.Admin && verifiedUsers[msg.sender]
+        );
+        _;
+    }
+
+    // Modifier to ensure only the verified super admin admin access a function
+    modifier verifiedSuperAdmin() {
+        require(
+            userRoles[msg.sender] == Role.SuperAdmin &&
+                verifiedUsers[msg.sender]
+        );
+        _;
+    }
+
+    // Initializing the Contract.
+    constructor() public {
+        creatorAdmin = msg.sender;
+        userRoles[creatorAdmin] = Role.SuperAdmin;
+        verifiedUsers[creatorAdmin] = true;
+    }
+
+    /// @dev Function to create property
+    /// @param _propId Identifier for property
+    /// @param _value Property Price
+    /// @param _owner Ownwe address property
+    function createProperty(
+        uint256 _propId,
+        uint256 _value,
+        address _owner
+    ) external verifiedAdmin verifiedUser(_owner) returns (bool)
+     {
+        properties[_propId] = BottleDetail(Status.Pending, _value, _owner);
+        return true;
+    }
+
+    /// @dev Approve property
+    /// @param _propId Identifier for property
+    function approveProperty(uint256 _propId)
+        external
+        verifiedSuperAdmin
+        returns (bool)
+    {
+        require(properties[_propId].currOwner != msg.sender);
+        properties[_propId].status = Status.Approved;
+        return true;
+    }
+
+    /// @dev Function to reject property
+    /// @param _propId Identifier for property
+    function rejectProperty(uint256 _propId)
+        external
+        verifiedSuperAdmin
+        returns (bool)
+    {
+        require(properties[_propId].currOwner != msg.sender);
+        properties[_propId].status = Status.Rejected;
+        return true;
+    }
+
+    /// @dev Function to change property ownership
+    /// @param _propId Identifier for property
+    /// @param _newOwner new Owner address for property
+    function changeOwnership(uint256 _propId, address _newOwner)
+        external
+        onlyOwner(_propId)
+        verifiedUser(_newOwner)
+        returns (bool)
+    {
+        require(properties[_propId].currOwner != _newOwner);
+        require(propOwnerChange[_propId] == address(0));
+        propOwnerChange[_propId] = _newOwner;
+        return true;
+    }
+
+    /// @dev Function to approve change of ownership
+    /// @param _propId Identifier for property
+    function approveChangeOwnership(uint256 _propId)
+        external
+        verifiedSuperAdmin
+        returns (bool)
+    {
+        require(propOwnerChange[_propId] != address(0));
+        properties[_propId].currOwner = propOwnerChange[_propId];
+        propOwnerChange[_propId] = address(0);
+        return true;
+    }
+
+    /// @dev Function to change property value
+    /// @param _propId Identifier for property
+    /// @param _newValue New Property Price
+    function changeValue(uint256 _propId, uint256 _newValue)
+        external
+        onlyOwner(_propId)
+        returns (bool)
+    {
+        require(propOwnerChange[_propId] == address(0));
+        properties[_propId].value = _newValue;
+        return true;
+    }
+
+    /// @dev Function to create property
+    /// @param _propId Identifier for property
+    function getPropertyDetails(uint256 _propId)
+        public
+        view
+        returns (
+            Status,
+            uint256,
+            address
+        )
+    {
+        return (
+            properties[_propId].status,
+            properties[_propId].value,
+            properties[_propId].currOwner
+        );
+    }
+
+    /// @dev Function to add a new user
+    /// @param _newUser new user address
+    function addNewUser(address _newUser)
+        external
+        verifiedAdmin
+        returns (bool)
+    {
+        require(userRoles[_newUser] == Role.Visitor);
+        require(verifiedUsers[_newUser] == false);
+        userRoles[_newUser] = Role.User;
+        return true;
+    }
+
+    /// @dev Function to add a new admin
+    /// @param _newAdmin new admin user address
+    function addNewAdmin(address _newAdmin)
+        external
+        verifiedSuperAdmin
+        returns (bool)
+    {
+        require(userRoles[_newAdmin] == Role.Visitor);
+        require(verifiedUsers[_newAdmin] == false);
+        userRoles[_newAdmin] = Role.Admin;
+        return true;
+    }
+
+    /// @dev Function to add a new admin
+    /// @param _newSuperAdmin new super admin user address
+    function addNewSuperAdmin(address _newSuperAdmin)
+        external
+        verifiedSuperAdmin
+        returns (bool)
+    {
+        require(userRoles[_newSuperAdmin] == Role.Visitor);
+        require(verifiedUsers[_newSuperAdmin] == false);
+        userRoles[_newSuperAdmin] = Role.SuperAdmin;
+        return true;
+    }
+
+    /// @dev Function to add a new admin
+    /// @param _newUser user address to approve
+    function approveUsers(address _newUser)
+        external
+        verifiedSuperAdmin
+        returns (bool)
+    {
+        require(userRoles[_newUser] != Role.Visitor);
+        verifiedUsers[_newUser] = true;
+        return true;
+    }
+
+    function initOwnershipTransfer(uint256 propertyId, address currentOwner, address newOwner) public {
+        require(properties[propertyId].currOwner == currentOwner, "Only the owner can initiate the ownership transfer.");
+        require(newOwner != address(0), "New owner cannot be the zero address.");
+        require(currentOwner != address(0), "Current owner cannot be the zero address.");
+        require(msg.sender == currentOwner, "Only the current owner can call this function.");
+        pendingOwner[propertyId] = newOwner;
+
+        emit OwnershipTransferInitiated(propertyId,currentOwner);
+    }
+    function transferOwnership(uint256 propertyId, address payable currentOwner, address payable newOwner) public payable {
+    require(properties[propertyId].currOwner == currentOwner, "Only the owner can initiate the ownership transfer.");
+    require(newOwner != address(0), "New owner cannot be the zero address.");
+    require(msg.sender == newOwner, "Only the new owner can call this function.");
+    uint256 balance = msg.sender.balance;
+    require(balance >= properties[propertyId].value, "Insufficient funds for the property value.");
+    require(pendingOwner[propertyId] == newOwner);
+
+    if(msg.value == properties[propertyId].value * 1 ether) {
+        properties[propertyId].currOwner = newOwner;
+        delete pendingOwner[propertyId];
+        } else {
+        payable(msg.sender).transfer(msg.value);
+
+
+    
+
+    emit OwnershipTransferAccepted(propertyId, newOwner);
+
+
+}
+    
+    }
+}
